f7cc80dede74921049324573e6a2c61317df33fc
Andy Davies <ad@andydavies.net>
2018-02-27 19:31:07 +0000

    Add tests for groupBy, startMark & endMark

--- __tests__/cli.test.js
+++ __tests__/cli.test.js
@@ -9,7 +9,7 @@ describe('Cli executable output', () => {
   it('default options snapshot test', () => {
     // http://riaevangelist.github.io/node-cmd/
     return new Promise( (resolve, reject) => {
-      cmd.get('node cli.js '+mockPath, (output) => {
+      cmd.get(`node cli.js ${mockPath}`, (output) => {
         expect(output).toMatchSnapshot()
         return resolve()
       })
@@ -37,6 +37,33 @@ describe('Cli executable output', () => {
   it('options --json snapshot test', () => {
     return new Promise( (resolve, reject) => {
       cmd.get(`node cli.js ${mockPath} --json`, (output) => {
+        expect(output).toMatchSnapshot()
+        return resolve()
+      })
+    })
+  })
+
+  it('options --groupBy EventName snapshot test', () => {
+    return new Promise( (resolve, reject) => {
+      cmd.get(`node cli.js ${mockPath} --groupBy EventName`, (output) => {
+        expect(output).toMatchSnapshot()
+        return resolve()
+      })
+    })
+  })
+
+  it('options --endMark firstPaint snapshot test', () => {
+    return new Promise( (resolve, reject) => {
+      cmd.get(`node cli.js ${mockPath} --endMark firstPaint`, (output) => {
+        expect(output).toMatchSnapshot()
+        return resolve()
+      })
+    })
+  })
+
+  it('options --startMark loadEventEnd --min-time=50 snapshot test', () => {
+    return new Promise( (resolve, reject) => {
+      cmd.get(`node cli.js ${mockPath} --startMark loadEventEnd --min-time=50`, (output) => {
         expect(output).toMatchSnapshot()
         return resolve()
       })
